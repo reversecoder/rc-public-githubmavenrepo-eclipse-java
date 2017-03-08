ba220129e45cf7d8e6f3f1f0a8681fc7739c18a7
Md. Rashadul Alam <rashedul.alam@kuusoft.com>
2017-03-08 20:09:17 +0600

    RC_PUBLIC_GITHUBMAVENREPO_ECLIPSE_JAVA: added public github hosted jar

--- src/main/java/com/reversecoder/public_githubmavenrepo_eclipse_java/publicGithubMavenRepoEclipseEJava.java
+++ src/main/java/com/reversecoder/public_githubmavenrepo_eclipse_java/publicGithubMavenRepoEclipseEJava.java
@@ -1,5 +1,10 @@
 package com.reversecoder.public_githubmavenrepo_eclipse_java;
 
+import com.reversecoder.samplelibrary.java.PleaseUseMe;
+
 public class publicGithubMavenRepoEclipseEJava {
 
+    public static void main(String arg[]) {
+        System.out.println("I am from library: " + PleaseUseMe.getInstance().pleasePleaseUseMe());
+    }
 }
